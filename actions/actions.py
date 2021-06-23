418b87945ecaf4a9441b28417f2ae27962bcff13
anhtubkhn <anhtubkhnk58@gmail.com>
2021-06-23 19:52:41 +0200

    DONE league_info, top-player, player-info

--- actions/actions.py
+++ actions/actions.py
@@ -8,7 +8,6 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
@@ -24,22 +23,24 @@ class ActionProvideLeagueInfo(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         league_name = tracker.get_slot("league_name")
-        # try:
-        print(f"ActionProvideLeagueInfo ---->>>>> league_name = {league_name}")
-        result = get_info.get_league_info(league_name)
-        if result is not None:
-            mess = ""
-            nb_matches = 0
-            for r in result:
-                mess += f"In {r['round']}:\n"
-                for fixture in r['fixtures']:
-                    mess += f"{fixture['home']['name']} meet {fixture['away']['name']} on {fixture['time']};\n"
-                    nb_matches += 1
-            mess = f"There are {nb_matches} matches coming up:\n" + mess
-        else:
-            mess = "The season is over"
-        # except:
-        #     mess = "Sorry! I cannot find information about this league"
+        try:
+            if league_name is not None:
+                print(f"ActionProvideLeagueInfo ---->>>>> league_name = {league_name}")
+                result = get_info.get_league_info(league_name)
+                if result is not None:
+                    mess = ""
+                    nb_matches = 0
+                    for r in result:
+                        mess += f"In {r['round']}:\n"
+                        for fixture in r['fixtures']:
+                            mess += f"{fixture['home']['name']} meet {fixture['away']['name']} on {fixture['time']};\n"
+                            nb_matches += 1
+                    mess = f"There are {nb_matches} matches coming up:\n" + mess
+                else:
+                    mess = "The season is over"
+        except Exception as e:
+            print(e)
+            mess = "Sorry! I cannot find information about this league"
         dispatcher.utter_message(mess)
         return [SlotSet("global_league_name", league_name)]
 
@@ -75,23 +76,27 @@ class ActionTopPlayer(Action):
             tracker: Tracker,
             domain: "DomainDict",
     ) -> List[Dict[Text, Any]]:
-        # try:
-        league_name = tracker.get_slot("league_name")
-        if league_name is None:
-            league_name = tracker.get_slot("global_league_name")
-        season = tracker.get_slot("season")
-        i_query_number = tracker.get_slot("query_number")
-        query_number = 1 if i_query_number is None else i_query_number
-        print(f"ActionTopPlayer ---->>>>> league_name = {league_name}({tracker.get_slot('league_name')}) season = {season} query_number = {query_number}({i_query_number})")
-        result = get_info.get_top_score(league_name, season, query_number)
-        if query_number == 1:
-            mess = f"The top player is {result[0]['name']} of {result[0]['team']} with {result[0]['goals']} goals"
-        else:
-            mess = f"The following is {query_number} top player\n"
-            for i in range(len(result)):
-                mess += f"{i} {result[i]['name']} of {result[i]['team']} with {result[i]['goals']} goals"
-        # except:
-        #     mess = "Sorry I cannot find this information"
+        try:
+            league_name = tracker.get_slot("league_name")
+            if league_name is None:
+                league_name = tracker.get_slot("global_league_name")
+            season = tracker.get_slot("season")
+            i_query_number = tracker.get_slot("query_number")
+            try:
+                query_number = int(i_query_number)
+            except:
+                query_number = 1
+            print(f"ActionTopPlayer ---->>>>> league_name = {league_name}({tracker.get_slot('league_name')}) season = {season} query_number = {query_number}({i_query_number})")
+            result = get_info.get_top_score(league_name, season, query_number)
+            if query_number == 1:
+                mess = f"The top player is {result[0]['name']} of {result[0]['team']} with {result[0]['goals']} goals"
+            else:
+                mess = f"The following is {query_number} top player:\n"
+                for i in range(len(result)):
+                    mess += f"{i+1}: {result[i]['name']} of {result[i]['team']} with {result[i]['goals']} goals.\n"
+        except Exception as e:
+            print(e)
+            mess = "Sorry I cannot find this information"
         dispatcher.utter_message(mess)
         return []
 
@@ -107,35 +112,36 @@ class ActionPlayerInfo(Action):
             domain: "DomainDict",
     ) -> List[Dict[Text, Any]]:
         print("========== ActionPlayerInfo ==========")
-        # try:
-        player_name = tracker.get_slot("person")
-        league_name = tracker.get_slot("spec_league_name")
-        season = tracker.get_slot("season")
-        nb_league = 1
-        if league_name is None:
-            nb_league = 0
-            # print(f"Slot {tracker.slots}")
-            league_name = tracker.get_slot("global_league_name")
+        try:
+            player_name = tracker.get_slot("PERSON")
+            league_name = tracker.get_slot("spec_league_name")
+            season = tracker.get_slot("season")
+            nb_league = 1
+            if league_name is None:
+                nb_league = 0
+                # print(f"Slot {tracker.slots}")
+                league_name = tracker.get_slot("global_league_name")
 
-        # TODO query_type is None just show info about player
-        query_type = tracker.get_slot("query_type")
+            # TODO query_type is None just show info about player
+            query_type = tracker.get_slot("query_type")
 
-        print(f"ActionPlayerInfo ---->>>>> player_name = {player_name} "
-              f"league_name = {league_name} query_type={query_type} nb_league = {nb_league}")
-        result = get_info.get_player_statistic(player_name, [league_name], season, nb_league, query_type)
-        print(f"ActionPlayerInfo ---->>>>> results = {result}")
-        # result = None
-        mess = ""
-        for league in result:
-            mess += f"At {league['league']}: "
-            for key, value in league[query_type].items():
-                if value is not None:
-                    mess += f"{key} is {value}; "
-            mess += "\n"
-    # except:
-        #     mess = "Sorry I cannot find information about this player"
+            print(f"ActionPlayerInfo ---->>>>> player_name = {player_name} "
+                  f"league_name = {league_name}({tracker.get_slot('spec_league_name')}) query_type={query_type} nb_league = {nb_league}")
+            result = get_info.get_player_statistic(player_name, [league_name], season, nb_league, query_type)
+            # print(f"ActionPlayerInfo ---->>>>> results = {result}")
+            # result = None
+            mess = ""
+            for league in result:
+                mess += f"At {league['league']}: "
+                for key, value in league[query_type].items():
+                    if value is not None:
+                        mess += f"{key} is {value}; "
+                mess += "\n"
+        except Exception as e:
+            print(e)
+            mess = "Sorry I cannot find information about this player"
         dispatcher.utter_message(mess)
-        return []
+        return [SlotSet("league_name", None), SlotSet("PERSON", None)]
 
 
 class ActionFixtures(Action):
